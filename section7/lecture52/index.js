b715c0ac5e70bcf1f323d41699a8df879ef5942c
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2026-01-20 08:45:18 -0300

    Agregar archivos y comandos para la creación, invocación y actualización de funciones Lambda en AWS

---
+++ section7/lecture52/index.js
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+    console.log('Event:', JSON.stringify(event, null, 2));
+    
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello from Lambda!',
+            input: event
+        })
+    };
+    return response;
+};
